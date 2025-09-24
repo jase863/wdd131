776d2b315e44d5cd029b63d0132ccdedd83b3a7d
Jason Walker <walker.jason8630@gmail.com>
2025-09-23 23:05:22 -0600

    finished page

--- scripts/country.js
+++ scripts/country.js
@@ -1,5 +1,29 @@
-const mobileIcon = "⛅";
-
 const date = new Date();
 document.querySelector("#currentyear").innerHTML = `&copy;${date.getFullYear()}`
 document.querySelector("#lastModified").innerHTML = `Last Modification: ${document.lastModified}`;
+
+let temp = document.querySelector(".temp");
+let conditions = document.querySelector(".conditions");
+let wind = document.querySelector(".wind");
+let chill = document.querySelector(".chill");
+let windSpeed = 16;
+let temperature = 5;
+
+temp.textContent = `Temperature: ${temperature} °C`;
+conditions.textContent = "Conditions: Partly Cloudy";
+wind.textContent = `Wind: ${windSpeed} km/h`;
+
+if (temperature <= 10 && windSpeed > 4.8) {
+    chill.textContent = `Wind Chill: ${calculateWindChill(windSpeed, temperature)} °C`;
+}
+
+else {
+    chill.textContent = "N/A";
+}
+
+function calculateWindChill(windSpeed, temperature){
+    let windchill = 13.12 + (.6215*temperature)-(11.37*(Math.pow(windSpeed,.16)))+(.3965*temperature*(Math.pow(windSpeed,.16)));
+
+    windchill = Math.round(windchill * 10) / 10;
+    return windchill;
+}
