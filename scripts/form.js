e4775220eb6f83ae8dee5f140ad668c6ed7d6515
Jason Walker <walker.jason8630@gmail.com>
2025-10-06 22:17:23 -0600

    Finished Form

---
+++ scripts/form.js
@@ -0,0 +1,47 @@
+const date = new Date();
+
+document.querySelector("#currentyear").innerHTML = `&copy;${date.getFullYear()}`
+
+document.querySelector("#lastModified").innerHTML = `Last Modification: ${document.lastModified}`;
+
+
+const products = [
+  {
+    id: "fc-1888",
+    name: "flux capacitor",
+    averagerating: 4.5
+  },
+  {
+    id: "fc-2050",
+    name: "power laces",
+    averagerating: 4.7
+  },
+  {
+    id: "fs-1987",
+    name: "time circuits",
+    averagerating: 3.5
+  },
+  {
+    id: "ac-2000",
+    name: "low voltage reactor",
+    averagerating: 3.9
+  },
+  {
+    id: "jj-1969",
+    name: "warp equalizer",
+    averagerating: 5.0
+  }
+];
+
+function CreateOption(products) {
+    const productSelection = document.getElementById("product-selection");
+    products.forEach(product => {
+        const option = document.createElement('option');
+        option.value = product.id;
+        option.textContent = product.name;
+
+        productSelection.appendChild(option);
+    });
+}
+
+CreateOption(products);
